712e88a31c5a822eb17cd60a1e45f7999f9af565
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-25 07:38:33 +0530

    feat: configured to show authlink code

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,13 @@
+<script lang="ts">
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+</script>
+{#if !(code)}
 <h1>
   Uh oh
 </h1>
 <p>You are supposed to be redirected to this page with a code!<br />That did not happen so apparently something is broken!</p>
+{:else}
+<h1>Got code to login!</h1>
+<p>Code: {code}</p>
+{/if}
