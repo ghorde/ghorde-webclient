ef7b2f656db0123467d540480fa1fcec26f07f6f
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-04-27 09:44:23 +0530

    fix: unreadable text on login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,12 +7,21 @@
     code = urlParams.get('code') || '' as string;
   });
 </script>
-{#if !(code) || code == ''}
-<h1>
-  Uh oh
-</h1>
-<p>You are supposed to be redirected to this page with a code!<br />That did not happen so apparently something is broken!</p>
-{:else}
-<h1>Got code to login!</h1>
-<p>Code: {code}</p>
-{/if}
+<main>
+  {#if !(code) || code == ''}
+  <h1>
+    Uh oh
+  </h1>
+  <p>You are supposed to be redirected to this page with a code!<br />That did not happen so apparently something is broken!</p>
+  {:else}
+  <h1>Got code to login!</h1>
+  <p>Code: {code}</p>
+  {/if}
+</main>
+
+<style lang="scss">
+  main {
+    padding-top: 156px;
+    text-align: center;
+  }
+</style>
