d5adffce9b2a4e1c256fac0e3e2ed0e8c116cd8d
Saksham-Shahu <sakshamshahu.work@gmail.com>
2023-04-28 19:32:30 +0530

    chore: format

--- src/lib/helpers/axios.ts
+++ src/lib/helpers/axios.ts
@@ -1,14 +1,14 @@
-import axios, { Axios } from "axios";
+import axios, { Axios } from 'axios';
 
 const startAxios: (baseURL: string) => Axios = (baseURL) => {
-  const axiosInstance = axios.create({
-    baseURL,
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    }
-  });
-  return axiosInstance;
+	const axiosInstance = axios.create({
+		baseURL,
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json'
+		}
+	});
+	return axiosInstance;
 };
 
 export const apiAxios = startAxios('https://ghorde-api.kodski.com/webclient/');
--- src/lib/helpers/storable.js
+++ src/lib/helpers/storable.js
@@ -1,33 +1,30 @@
-import { get, writable } from 'svelte/store'
-
+import { get, writable } from 'svelte/store';
 
 /**
  * @param {any} data
  */
 export function storable(data) {
-   const store = writable(data);
-   const { subscribe, set, update } = store;
-   const isBrowser = typeof window !== 'undefined';
+	const store = writable(data);
+	const { subscribe, set, update } = store;
+	const isBrowser = typeof window !== 'undefined';
 
-   isBrowser &&
-      localStorage.storable &&
-      set(JSON.parse(localStorage.storable));
+	isBrowser && localStorage.storable && set(JSON.parse(localStorage.storable));
 
-   return {
-      subscribe,
-      set: (/** @type {any} */ n) => {
-         if(isBrowser) {
-          localStorage.storable = JSON.stringify(n)
-         }
-         set(n);
-      },
-      update: (/** @type {(arg0: any) => any} */ cb) => {
-         const updatedStore = cb(get(store));
+	return {
+		subscribe,
+		set: (/** @type {any} */ n) => {
+			if (isBrowser) {
+				localStorage.storable = JSON.stringify(n);
+			}
+			set(n);
+		},
+		update: (/** @type {(arg0: any) => any} */ cb) => {
+			const updatedStore = cb(get(store));
 
-         if(isBrowser){
-          (localStorage.storable = JSON.stringify(updatedStore));
-         }
-         set(updatedStore);
-      }
-   };
+			if (isBrowser) {
+				localStorage.storable = JSON.stringify(updatedStore);
+			}
+			set(updatedStore);
+		}
+	};
 }
