15906a7c963188f9326c1413c048eb18b3163f61
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-06-07 06:57:44 +0530

    fix: button redirect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	import { page } from '$app/stores';
 
 	const handleLoginClick = () => {
-		goto('https://cardboard.ink/a/glt');
+		goto('https://cardboard.ink/a/ghorde');
 	}
 
 	let userActionsVisible = false;
